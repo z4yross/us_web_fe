0a2b4319447f8d574223b2aa810df5ad06e0046e
z4yross <z4yross@gmail.com>
2022-06-07 20:24:36 -0500

    logint

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+
+import { FiAtSign, FiCheck, FiKey, FiSend } from "react-icons/fi";
 
 const Login = () => {
-    return (
-        <div>
+	const [loading, setLoading] = useState(false);
 
-        </div>
-    );
+    const [error, setError] = useState("Por favor ingrese su correo y contraseña.")
+    const [isError, setIsError] = useState(false)
+
+	const send = () => {
+        setLoading(false)
+		setLoading(true)
+	};
+
+    useEffect(() => {
+        if(isError){
+            setLoading(false);
+        }
+    }, [isError])
+
+	return (
+		<div className="Login">
+            <div className={`InfoLabelContainer ${isError ? "ErrorLabel": ""}`}>
+                <label>{error}</label>
+            </div>
+			<div className="EmailContainer">
+				<FiAtSign />
+				<input type="email" placeholder="Correo" />
+			</div>
+			<div className="PasswordContainer">
+				<FiKey />
+				<input type="password" placeholder="Contraseña" />
+			</div>
+			<button
+				className={`SubmitLoginButton ${loading ? "loading" : ""}`}
+				onClick={send}
+			>
+				<label>ENTRAR</label>
+				<FiSend />
+			</button>
+		</div>
+	);
 };
 
 export default Login;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+
+import { FiAtSign, FiUser, FiKey, FiSend } from "react-icons/fi";
 
 const Signup = () => {
-    return (
-        <div>
+	const [loading, setLoading] = useState(false);
 
-        </div>
-    );
+	const [error, setError] = useState(
+		"Por favor ingrese su informacion."
+	);
+	const [isError, setIsError] = useState(false);
+
+	const send = () => {
+		setLoading(false);
+		setLoading(true);
+	};
+
+	useEffect(() => {
+		if (isError) {
+			setLoading(false);
+		}
+	}, [isError]);
+
+	return (
+		<div className="Login">
+			<div
+				className={`InfoLabelContainer ${isError ? "ErrorLabel" : ""}`}
+			>
+				<label>{error}</label>
+			</div>
+			<div className="UserNameContainer">
+				<FiUser />
+				<input type="text" placeholder="Nombre de Usuario" />
+			</div>
+			<div className="EmailContainer">
+				<FiAtSign />
+				<input type="email" placeholder="Correo" />
+			</div>
+			<div className="PasswordContainer">
+				<FiKey />
+				<input type="password" placeholder="Contraseña" />
+			</div>
+			<div className="PasswordContainer">
+				<FiKey />
+				<input type="password" placeholder="Repita la contraseña" />
+			</div>
+			<button
+				className={`SubmitLoginButton ${loading ? "loading" : ""}`}
+				onClick={send}
+			>
+				<label>REGISTRO</label>
+				<FiSend />
+			</button>
+		</div>
+	);
 };
 
 export default Signup;
